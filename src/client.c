df336a95e2a9695075766fe0930d3a6dfb806c61
Isaac Morneau <isaacmorneau@gmail.com>
2018-02-06 20:35:06 +0000

    temp add client scaffold

--- src/client.c
+++ src/client.c
@@ -16,7 +16,22 @@
 
 #define MAXEVENTS 256
 
-void client(const char rconst char * port) {
+void add_client_con(const char * address, const char * port, struct epoll_event * efd) {
+    struct epoll_event event;
+    connection * con;
+
+    con = (connection *)calloc(1, sizeof(connection));
+
+    init_connection(conn, make_connected(address, port));
+    //cant add EPOLLRDHUP as EPOLLEXCLUSIVE would then fail
+    //instead check for a read of 0
+    event.events = EPOLLIN | EPOLLOUT | EPOLLEXCLUSIVE | EPOLLET;
+    event.data.ptr = con;
+    //TODO check this as we may need to calloc theevent
+    check(epoll_ctl(*efd, EPOLL_CTL_ADD, con->sockfd, &event) != -1);
+}
+
+void client(const char * address,  const char * port) {
     int efd;
     struct epoll_event event;
     struct epoll_event *events;
@@ -31,21 +46,18 @@ void client(const char rconst char * port) {
         n = epoll_wait(efd, events, MAXEVENTS, -1);
         for (i = 0; i < n; i++) {
             if ((events[i].events & EPOLLERR) ||
-                    (events[i].events & EPOLLHUP)) {
-                // An error has occured on this fd, or the socket is not
-                // ready for reading (why were we notified then?)
-                perror("epoll_wait, unexpected error or close");
+                    (events[i].events & EPOLLHUP)) { // error or unexpected close
+                perror("epoll_wait");
                 close_connection(events[i].data.ptr);
                 continue;
-            } else if (events[i].events & EPOLLDHUP) {//connection closed
-                close_connection(events[i].data.ptr);
-                continue;
-            } else if (events[i].events & EPOLLIN) {//data has been echoed back
+            } else if (events[i].events & EPOLLIN) {//data has been echoed back or remote has closed connection
                 //TODO record read bytes
-                black_hole_read((connectio *)events[i].data.ptr);
+                int bytes = black_hole_read((connection *)events[i].data.ptr);
+                if (bytes == 0 && (errno != EAGAIN || errno != EWOULDBLOCK))
+                    close_connection(events[i].data.ptr);
                 continue;
             } else if (events[i].events & EPOLLOUT) {//data can be written
-                send_pipe((connectio *)events[i].data.ptr);
+                send_pipe((connection *)events[i].data.ptr);
                 continue;
             }
         }
