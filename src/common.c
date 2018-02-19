c867eb4f8025a38d98dcde484365835e988dbd29
Isaac <isaacmorneau@gmail.com>
2018-02-18 23:00:02 -0800

    fixed rlimit

--- src/common.c
+++ src/common.c
@@ -3,7 +3,7 @@
 
 void set_fd_limit() {
     struct rlimit lim;
-    lim.rlim_cur = RLIM_INFINITY;
-    lim.rlim_max = RLIM_INFINITY;
+    ensure(getrlimit(RLIMIT_NOFILE, &lim) != -1);
+    lim.rlim_cur = lim.rlim_max;
     ensure(setrlimit(RLIMIT_NOFILE, &lim) != -1);
 }
