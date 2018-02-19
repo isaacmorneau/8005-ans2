327661d9ea2a5c8f902b673b297bd350d4277407
Isaac <isaacmorneau@gmail.com>
2018-02-19 08:36:16 -0800

    fixed limits and increased to USHRT_MAX

--- src/common.c
+++ src/common.c
@@ -1,9 +1,13 @@
 #include <sys/resource.h>
+#include <limits.h>
 #include "common.h"
 
 void set_fd_limit() {
     struct rlimit lim;
-    ensure(getrlimit(RLIMIT_NOFILE, &lim) != -1);
-    lim.rlim_cur = lim.rlim_max;
+    //ensure(getrlimit(RLIMIT_NOFILE, &lim) != -1);
+    //the kernel patch that allows for RLIM_INFINITY to work breaks stuff
+    //so we are restricted to finite values
+    lim.rlim_cur = USHRT_MAX;
+    lim.rlim_max = USHRT_MAX;
     ensure(setrlimit(RLIMIT_NOFILE, &lim) != -1);
 }
